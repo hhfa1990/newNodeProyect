74a7d051705da27d74a2ab6de9996dac0d7e7f67
Heddie Franco <hhfa1990@gmail.com>
2014-04-06 14:12:37 -0500

    test

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app);
+
+server.listen(3000, 'localhost');
+
+console.log('Servidor corriendo en localhost:3000');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static('public'));
+
+
+app.get('/', function(req, res){
+    res.render('index', {
+    	title: 'Home',
+    	h1: 'Hello World!'
+    });
+});
